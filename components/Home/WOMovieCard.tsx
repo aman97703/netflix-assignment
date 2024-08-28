43a355e5c2cd9f3d0ec34902a1d0c869913f8186
Aman Gupta <aman97703@gmail.com>
2024-08-28 15:35:51 +0530

    update

--- components/Home/WOMovieCard.tsx
+++ components/Home/WOMovieCard.tsx
@@ -11,12 +11,12 @@ interface WOMovieCardParams {
 const WOMovieCard = ({ video }: WOMovieCardParams) => {
   return (
     <div className="group bg-zinc-900 col-span relative md:h-[12vw] h-[100px] p-2">
-      <img
+      <Image
         src={video.thumbnailUrl}
         alt={video.title}
         className="w-full cursor-pointer object-cover transition duration-0 rounded-md group-hover:opacity-90 sm:group-hover:opacity-0 delay-300 md:h-[12vw] h-[100px]"
-        // width={200}
-        // height={100}
+        width={200}
+        height={100}
       />
       <div className="opacity-0 absolute top-0 duration-200 transition z-10 invisible sm:visible delay-300 w-[150%] scale-0 group-hover:scale-110 group-hover:-translate-y-[6vw]  group-hover:opacity-100">
         <Image
